832cfe5bd5039340a1f338b59c78d67530fb2c33
unknown <gottfriedcpn@gmail.com>
2024-02-13 10:29:28 +0700

    max file upl size

--- jdih_project/settings.py
+++ jdih_project/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticroot"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "mediaroot"
+FILE_UPLOAD_MAX_MEMORY_SIZE =  50 *  1024 *  1024  #  5 MB
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
